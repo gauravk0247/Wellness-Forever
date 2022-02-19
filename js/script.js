7cce095ee4ed5688a4153ad5d3c2c61eaf8d5fa7
gauravk0247 <86191685+gauravk0247@users.noreply.github.com>
2022-02-19 17:01:22 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:14,
+    autoplay:true,
+    stagePadding: 50,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
